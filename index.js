4bf84aa32e1b4925735a05e134480be3d255e271
vikas jadaun <deadpoolji125@gmail.com>
2023-06-26 23:36:05 +0530

    update

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ var MenuItem = document.getElementById("sidebar");
 MenuItem.style.display = "none";
 
 function menutoggle() {
-     
+     if (MenuItem.style.display == "none")
+      MenuItem.style.display = "block";
+     else {
+       MenuItem.style.display = "none";
+     }
    }
