0beec27e939d21f1070bfa508e67852c576675d1
vikas jadaun <deadpoolji125@gmail.com>
2023-06-26 23:50:16 +0530

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var MenuItem = document.getElementById("nav");
 MenuItem.style.display = "flex";
 
 function menutoggle() {
-     if (MenuItem.style.display == "none")
+     if (MenuItem.style.display == "flex")
       MenuItem.style.display = "block";
      else {
        MenuItem.style.display = "none";
