1c09a507b9a93f3b290b86910e9ffe26584df2c9
vikas jadaun <deadpoolji125@gmail.com>
2023-06-27 11:52:30 +0530

    update

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-// var MenuItem = document.getElementById("nav");
-
-// MenuItem.style.display = "flex";
-
-// function menutoggle() {
-//      if (MenuItem.style.display == "none")
-//       MenuItem.style.display = "block";
-//      else {
-//        MenuItem.style.display = "none";
-//      }
-//    }
-
 // toggle button 
-
 
    const toggleButton = document.getElementsByClassName('fa-bars')[0]
     const navbarLinks = document.getElementsByClassName('nav')[0]
@@ -19,3 +6,6 @@
     toggleButton.addEventListener('click', () => {
         navbarLinks.classList.toggle('active')
     })
+
+
+    // toggle button 
